5e5c97388f4ab5e2700b48037e57def2ed9653ec
Ilango <rajagopalilango@gmail.com>
2021-11-12 23:24:16 +0530

    👌 IMPROVE: Update lint-staged command

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,45 +16,45 @@ import { CTA } from '../components/CTA';
 import { Footer } from '../components/Footer';
 
 const Index = () => (
-  <Container height='100vh'>
-	<Hero />
-	  <Main>
-		<Text>
-	      Example repository of <Code>Next.js</Code> +{' '}
-		  <Code>chakra-ui</Code> + <Code>TypeScript</Code>.
-		</Text>
+	<Container height='100vh'>
+		<Hero />
+		<Main>
+			<Text>
+				Example repository of <Code>Next.js</Code> +{' '}
+				<Code>chakra-ui</Code> + <Code>TypeScript</Code>.
+			</Text>
 
-		<List spacing={3} my={0}>
-		  <ListItem>
-			<ListIcon as={CheckCircleIcon} color='green.500' />
-			<ChakraLink
-			  isExternal
-			  href='https://chakra-ui.com'
-			  flexGrow={1}
-			  mr={2}
-			>
-			  Chakra UI <LinkIcon />
-			</ChakraLink>
-		  </ListItem>
-		  <ListItem>
-			<ListIcon as={CheckCircleIcon} color='green.500' />
-		      <ChakraLink
-				isExternal
-				href='https://nextjs.org'
-				flexGrow={1}
-				mr={2}
-			  >
-				Next.js <LinkIcon />
-				</ChakraLink>
-		  </ListItem>
-		</List>
-	  </Main>
+			<List spacing={3} my={0}>
+				<ListItem>
+					<ListIcon as={CheckCircleIcon} color='green.500' />
+					<ChakraLink
+						isExternal
+						href='https://chakra-ui.com'
+						flexGrow={1}
+						mr={2}
+					>
+						Chakra UI <LinkIcon />
+					</ChakraLink>
+				</ListItem>
+				<ListItem>
+					<ListIcon as={CheckCircleIcon} color='green.500' />
+					<ChakraLink
+						isExternal
+						href='https://nextjs.org'
+						flexGrow={1}
+						mr={2}
+					>
+						Next.js <LinkIcon />
+					</ChakraLink>
+				</ListItem>
+			</List>
+		</Main>
 
-	  <DarkModeSwitch />
-	  <Footer>
-		<Text>Next ❤️ Chakra</Text>
-	  </Footer>
-	  <CTA />
+		<DarkModeSwitch />
+		<Footer>
+			<Text>Next ❤️ Chakra</Text>
+		</Footer>
+		<CTA />
 	</Container>
 );
 
