0c0408ff9838c0ce3f8592e73b2b1886e0f2cc13
Ilango <rajagopalilango@gmail.com>
2021-11-18 10:36:31 +0530

    📦 NEW: Use a less distracting sign in button and add copy to sign in modal on why users should sign up to the service

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,7 @@ import {
 	Flex,
 	Grid,
 	GridItem,
+	Heading,
 	HStack,
 	FormControl,
 	FormLabel,
@@ -16,6 +17,9 @@ import {
 	IconButton,
 	Input,
 	Link,
+	List,
+	ListIcon,
+	ListItem,
 	Menu,
 	MenuButton,
 	MenuList,
@@ -44,8 +48,12 @@ import {
 	useDisclosure,
 	useToast,
 } from '@chakra-ui/react';
-import { Download, Edit3, Moon, Square, Sun, User } from 'react-feather';
-import { MdCenterFocusStrong, MdOutlineSpaceDashboard } from 'react-icons/md';
+import { Download, Edit3, LogIn, Moon, Square, Sun, User } from 'react-feather';
+import {
+	MdCenterFocusStrong,
+	MdCheckCircle,
+	MdOutlineSpaceDashboard,
+} from 'react-icons/md';
 import { supabase } from '../lib/supabase';
 import Timer from './Timer';
 import AuthBasic from './Auth';
@@ -659,11 +667,18 @@ export default function Header(props) {
 						</Menu>
 					) : (
 						<Button
-							colorScheme='brand'
+							aria-label='sign in'
+							w={10}
+							h={10}
+							p={0}
+							rounded='md'
+							d='flex'
+							align='center'
+							justify='center'
+							variant='ghost'
 							onClick={onAuthModalOpen}
-							size='sm'
 						>
-							Sign In
+							<Icon as={LogIn} />
 						</Button>
 					)}
 				</HStack>
@@ -672,23 +687,112 @@ export default function Header(props) {
 				isCentered={true}
 				isOpen={isAuthModalOpen}
 				onClose={onAuthModalClose}
+				size='5xl'
 			>
 				<ModalOverlay />
 				<ModalContent>
-					<ModalBody py={6}>
-						<Flex
-							w='full'
-							alignItems='center'
-							justifyContent='center'
-							mb={8}
-						>
-							<NextImage
-								src='/images/shield.png'
-								width={96}
-								height={96}
-							/>
-						</Flex>
-						<AuthBasic />
+					<ModalBody py={12}>
+						<Grid gap={4} templateColumns='repeat(2, 1fr)'>
+							<VStack
+								h='full'
+								alignItems='start'
+								justifyContent='center'
+								color={useColorModeValue('black', 'white')}
+								px={8}
+								spacing={8}
+							>
+								<Heading as='h2' fontSize='2xl'>
+									By signing up, you can:
+								</Heading>
+								<List spacing={4}>
+									<ListItem d='flex' alignItems='start'>
+										<ListIcon
+											as={MdCheckCircle}
+											color='green.500'
+											fontSize='xl'
+											mt={1}
+										/>
+										<VStack alignItems='start'>
+											<Heading as='h3' fontSize='xl'>
+												Sync to the cloud
+											</Heading>
+											<Text fontSize='sm'>
+												Save all your writing to the
+												cloud and access them anywhere
+											</Text>
+										</VStack>
+									</ListItem>
+									<ListItem d='flex' alignItems='start'>
+										<ListIcon
+											as={MdCheckCircle}
+											color='green.500'
+											fontSize='xl'
+											mt={1}
+										/>
+										<VStack alignItems='start'>
+											<Heading as='h3' fontSize='xl'>
+												Track your writing goals
+											</Heading>
+											<Text fontSize='sm'>
+												Get detailed analytics from your
+												writing sessions and stay
+												consistent
+											</Text>
+										</VStack>
+									</ListItem>
+									<ListItem d='flex' alignItems='start'>
+										<ListIcon
+											as={MdCheckCircle}
+											color='green.500'
+											fontSize='xl'
+											mt={1}
+										/>
+										<VStack alignItems='start'>
+											<Heading as='h3' fontSize='xl'>
+												Shareable Links
+											</Heading>
+											<Text fontSize='sm'>
+												Get shareable links to your
+												posts and get feedback from your
+												peers.
+											</Text>
+										</VStack>
+									</ListItem>
+									<ListItem d='flex' alignItems='start'>
+										<ListIcon
+											as={MdCheckCircle}
+											color='green.500'
+											fontSize='xl'
+											mt={1}
+										/>
+										<VStack alignItems='start'>
+											<Heading as='h3' fontSize='xl'>
+												And more...
+											</Heading>
+											<Text fontSize='sm'>
+												I&apos;m constantly adding
+												features that help you become a
+												better writer.
+											</Text>
+										</VStack>
+									</ListItem>
+								</List>
+							</VStack>
+							<VStack px={8} spacing={8}>
+								<Flex
+									w='full'
+									alignItems='center'
+									justifyContent='center'
+								>
+									<NextImage
+										src='/images/shield.png'
+										width={96}
+										height={96}
+									/>
+								</Flex>
+								<AuthBasic />
+							</VStack>
+						</Grid>
 					</ModalBody>
 				</ModalContent>
 			</Modal>
