616eda06d371ea71eae679ce3888c66b476880ef
Ilango <rajagopalilango@gmail.com>
2021-11-19 20:39:06 +0530

    📦 NEW: Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import { AppProps } from 'next/app';
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 import theme from '../theme';
@@ -6,18 +7,39 @@ import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider resetCSS theme={theme}>
-			<ColorModeProvider
-				options={{
-					useSystemColorMode: true,
+		<>
+			<Script
+				strategy='afterInteractive'
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+			<Script
+				id='google-analytics'
+				strategy='afterInteractive'
+				dangerouslySetInnerHTML={{
+					__html: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						
+						gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+							page_path: window.location.pathname,
+						});
+					`,
 				}}
-			>
-				<Head>
-					<title>Aurelius</title>
-				</Head>
-				<Component {...pageProps} />
-			</ColorModeProvider>
-		</ChakraProvider>
+			/>
+			<ChakraProvider resetCSS theme={theme}>
+				<ColorModeProvider
+					options={{
+						useSystemColorMode: true,
+					}}
+				>
+					<Head>
+						<title>Aurelius</title>
+					</Head>
+					<Component {...pageProps} />
+				</ColorModeProvider>
+			</ChakraProvider>
+		</>
 	);
 }
 
