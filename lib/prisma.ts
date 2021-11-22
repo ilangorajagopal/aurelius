fb433b5ffd88194e7f8eb07ccb709788ffd517ad
Ilango <rajagopalilango@gmail.com>
2021-11-22 08:28:34 +0530

    🐛 FIX: Another try to fix this pesky deployment error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
-export * from '@prisma/client';
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
-	prisma = new PrismaClient({ errorFormat: 'minimal' });
+	prisma = new PrismaClient();
 } else {
-	globalThis['prisma'] =
-		globalThis['prisma'] || new PrismaClient({ errorFormat: 'pretty' });
-	prisma = globalThis['prisma'];
+	if (!global.prisma) {
+		global.prisma = new PrismaClient();
+	}
+	prisma = global.prisma;
 }
-
 export default prisma;
