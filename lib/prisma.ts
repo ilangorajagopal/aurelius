03e22339bdab13c65e4f9f3417ecad2e8ce97c61
Ilango <rajagopalilango@gmail.com>
2021-11-22 08:31:40 +0530

    🐛 FIX: Yet another try to fix the deployment error. It's getting on my nerves. Just deploy already

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,9 +11,9 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
 	prisma = new PrismaClient();
 } else {
-	if (!global.prisma) {
-		global.prisma = new PrismaClient();
+	if (!global?.prisma) {
+		global?.prisma = new PrismaClient();
 	}
-	prisma = global.prisma;
+	prisma = global?.prisma;
 }
 export default prisma;
