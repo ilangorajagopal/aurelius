92d459921a4a04c37ec8dc01c9726712cfebdee3
Ilango <rajagopalilango@gmail.com>
2021-11-22 08:47:39 +0530

    🐛 FIX: Hopefully this will be the last. Please let this be the last.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,14 +6,14 @@ import { PrismaClient } from '@prisma/client';
 // Learn more:
 // https://pris.ly/d/help/next-js-best-practices
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-	prisma = new PrismaClient();
-} else {
-	if (!global?.prisma) {
-		global?.prisma = new PrismaClient();
-	}
-	prisma = global?.prisma;
+declare global {
+	let prisma: PrismaClient | undefined;
 }
-export default prisma;
+
+export const prisma =
+	global?.prisma ||
+	new PrismaClient({
+		log: ['query'],
+	});
+
+if (process.env.NODE_ENV !== 'production') global?.prisma = prisma;
