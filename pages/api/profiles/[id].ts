c6e55dfed07e6a6ac94554d610860a67ee1b84e5
Ilango <rajagopalilango@gmail.com>
2021-11-24 08:36:13 +0530

    📦 NEW: Add api routes for user profiles

---
+++ pages/api/profiles/[id].ts
@@ -0,0 +1,40 @@
+import nc from 'next-connect';
+import prisma from '../../../lib/prisma';
+
+async function getOne(req, res) {
+	const { id } = req.query;
+
+	const user = await prisma.user.findUnique({
+		where: {
+			id,
+		},
+	});
+	res.status(200).json({ user });
+}
+
+async function update(req, res) {
+	const { id } = req.query;
+	const data = req.body.data;
+	const updatedUser = await prisma.user.update({
+		data,
+		where: {
+			id,
+		},
+	});
+	res.status(200).json({ message: 'user_updated', user: updatedUser });
+}
+
+// async function deleteOne(req, res) {
+//     const { id } = req.query;
+//     const deletedUser = await prisma.user.delete({
+//         where: {
+//             id,
+//         },
+//         select: {
+//             email: true,
+//         },
+//     });
+//     res.status(200).json({ message: 'user_deleted', user: deletedUser });
+// }
+
+export default nc({ attachParams: true }).get(getOne).put(update);
