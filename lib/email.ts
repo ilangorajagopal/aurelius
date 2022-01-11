62e1e93686e01d307ffa878e7c78587d5181c32b
Ilango <rajagopalilango@gmail.com>
2022-01-11 08:55:56 +0530

    👌 IMPROVE: Check if name is available in email.ts

--- lib/email.ts
+++ lib/email.ts
@@ -3,7 +3,7 @@ import { sendgrid } from './sendgrid';
 export async function addNewUserToContacts(user) {
 	const { email, name } = user;
 	const [first_name, last_name] =
-		name.split(' ').length > 1 ? name.split(' ') : [name, ''];
+		name && name.split(' ').length > 1 ? name.split(' ') : [name, ''];
 	const data = {
 		list_ids: ['5a681261-e0e5-42ec-9eeb-ea514fc5b4ba'],
 		contacts: [
