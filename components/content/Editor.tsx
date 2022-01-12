e98f19dc37628f15d4348d77144fa13d5c7a9534
Ilango <rajagopalilango@gmail.com>
2022-01-12 10:41:14 +0530

    📦 NEW: Add option to upload image in editor

--- components/content/Editor.tsx
+++ components/content/Editor.tsx
@@ -1,5 +1,14 @@
-import { Box } from '@chakra-ui/react';
-import { BubbleMenu, EditorContent } from '@tiptap/react';
+import {
+	Box,
+	IconButton,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	Text,
+} from '@chakra-ui/react';
+import { BubbleMenu, EditorContent, FloatingMenu } from '@tiptap/react';
+import { Image, Plus, Twitter, Youtube } from 'react-feather';
 import Toolbar from './Toolbar';
 
 export default function Editor(props) {
@@ -8,9 +17,44 @@ export default function Editor(props) {
 	return (
 		<Box w='full' h='auto' minH='600px'>
 			{editor && (
-				<BubbleMenu editor={editor}>
-					<Toolbar editor={editor} />
-				</BubbleMenu>
+				<>
+					<BubbleMenu editor={editor}>
+						<Toolbar editor={editor} />
+					</BubbleMenu>
+					<FloatingMenu editor={editor}>
+						<Menu>
+							<MenuButton
+								as={IconButton}
+								icon={<Plus width={20} height={20} />}
+								p={0}
+								w={8}
+								minW={8}
+								h={8}
+								borderWidth={1}
+								borderStyle='solid'
+								rounded='full'
+								variant='ghost'
+								position='absolute'
+								top='-1.1rem'
+								left='-4.2rem'
+							/>
+							<MenuList p={0}>
+								<MenuItem w='full' h={12}>
+									<Image width={16} height={16} />
+									<Text ml={4}>Image</Text>
+								</MenuItem>
+								{/*<MenuItem w='full' h={12}>*/}
+								{/*	<Youtube width={16} height={16} />*/}
+								{/*	<Text ml={4}>Youtube</Text>*/}
+								{/*</MenuItem>*/}
+								{/*<MenuItem w='full' h={12}>*/}
+								{/*	<Twitter width={16} height={16} />*/}
+								{/*	<Text ml={4}>Tweet</Text>*/}
+								{/*</MenuItem>*/}
+							</MenuList>
+						</Menu>
+					</FloatingMenu>
+				</>
 			)}
 			<EditorContent editor={props?.editor} />
 		</Box>
