ccf50d1acbd3395640c1f97628c5489c38c063f1
Ilango <rajagopalilango@gmail.com>
2022-01-16 09:55:39 +0530

    🐛 FIX: Update env variables for AWS S3 access

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,6 +1,12 @@
 import aws from 'aws-sdk';
-const REGION = process.env.AWS_REGION;
-aws.config.update({ region: REGION });
+const REGION = process.env.MY_AWS_DEFAULT_REGION;
+const AWS_ACCESS_KEY_ID = process.env.MY_AWS_ACCESS_KEY_ID;
+const AWS_SECRET_ACCESS_KEY = process.env.MY_AWS_SECRET_ACCESS_KEY;
+aws.config.update({
+	accessKeyId: AWS_ACCESS_KEY_ID,
+	secretAccessKey: AWS_SECRET_ACCESS_KEY,
+	region: REGION,
+});
 const s3Client = new aws.S3();
 
 export { s3Client };
