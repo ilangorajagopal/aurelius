3bbfd421676662dc647c248dab5b46c167fc0af2
Ilango <rajagopalilango@gmail.com>
2022-05-10 10:33:44 +0530

    📦 NEW: Add images from main branch

--- app/root.tsx
+++ app/root.tsx
@@ -23,12 +23,47 @@ export const links: LinksFunction = () => {
 			href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;600;700&family=Merriweather:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap',
 		},
 		{ rel: 'stylesheet', href: styles },
+		{ rel: 'manifest', href: '/site.webmanifest' },
+		{
+			rel: 'apple-touch-icon',
+			sizes: '180x180',
+			href: '/apple-touch-icon.png',
+		},
+		{
+			rel: 'icon',
+			type: 'image/png',
+			sizes: '32x32',
+			href: '/favicon-32x32.png',
+		},
+		{
+			rel: 'icon',
+			type: 'image/png',
+			sizes: '16x16',
+			href: '/favicon-16x16.png',
+		},
+		{ rel: 'mask-icon', color: '#5bbad5', href: '/safari-pinned-tab.svg' },
 	]
 }
 
 export const meta: MetaFunction = () => ({
 	charset: 'utf-8',
-	title: 'New Remix App',
+	'msapplication-TileColor': '#2b5797',
+	'og:site': 'https://aurelius.ink',
+	'og:url': 'https://aurelius.ink',
+	'og:title': 'Aurelius',
+	'og:description':
+		'Beautiful, minimal writing app. Eliminate distractions when writing, build a writing habit, track your daily writing goal, and more.',
+	'og:image': '/images/aurelius_open_graph.png',
+	'theme-color': '#ffffff',
+	title: 'Aurelius',
+	'twitter:card': 'summary_large_image',
+	'twitter:site': '@_ilango',
+	'twitter:url': 'https://aurelius.ink/',
+	'twitter:creator': '@_ilango',
+	'twitter:title': 'Aurelius',
+	'twitter:description':
+		'Beautiful, minimal writing app. Eliminate distractions when writing, build a writing habit, track your daily writing goal, and more.',
+	'twitter:image': 'https://www.aurelius.ink/images/aurelius_open_graph.png',
 	viewport: 'width=device-width,initial-scale=1',
 })
 
@@ -39,7 +74,7 @@ const Document = (props: DocumentProps) => {
 				<Meta />
 				<Links />
 			</head>
-			<body>
+			<body className='h-full w-full bg-brand-900'>
 				{props.children}
 				<ScrollRestoration />
 				<Scripts />
