dcf0c0291b7afc6d74b551af6d15a3b653aa5c1e
Ilango <rajagopalilango@gmail.com>
2022-05-12 11:17:52 +0530

    👌 IMPROVE: Update home page styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,18 +6,23 @@ export default function Index() {
 		<div className='flex h-full w-full items-start justify-center'>
 			<div className='container flex h-full w-full flex-col items-center justify-center space-y-8 text-white'>
 				<img
-					className='w-64'
+					className='w-48'
 					src='/images/logo_dark.png'
 					alt='Aurelius Logo'
 				/>
-				<p className='w-2/3 text-center text-lg leading-8 md:w-1/2'>
-					Beautiful, minimal writing app. Eliminate distractions when
-					writing, build a writing habit, track your daily writing
-					goal, and more.
-				</p>
+				<div className='flex w-full flex-col items-center justify-center'>
+					<p className='text-center text-lg leading-8 text-white md:w-1/2'>
+						Beautiful, minimal writing app that helps build a
+						writing habit.
+					</p>
+					<p className='text-center text-lg leading-8 text-gray-400 md:w-1/2'>
+						Eliminate distractions when writing, track your daily
+						writing goal, and more.
+					</p>
+				</div>
 				<div className='flex items-center justify-center space-x-4'>
 					<Link to='/app'>
-						<button className='text-md inline-flex justify-center rounded-md border border-transparent bg-brand-500 px-4 py-2 font-semibold text-brand-800 transition-all duration-200 hover:bg-brand-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-brand-500 focus-visible:ring-offset-2'>
+						<button className='text-md inline-flex hidden justify-center rounded-md border border-transparent bg-brand-500 px-4 py-2 font-semibold text-brand-800 transition-all duration-200 hover:bg-brand-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-brand-500 focus-visible:ring-offset-2'>
 							Start Writing
 						</button>
 					</Link>
@@ -26,7 +31,7 @@ export default function Index() {
 						href='https://twitter.com/aureliusdotink'
 						rel='noreferrer'
 					>
-						<button className='flex items-center justify-center rounded-md bg-transparent px-4 py-2 text-base font-medium leading-8 text-brand-500'>
+						<button className='flex items-center justify-center rounded-md bg-transparent px-4 py-2 text-base font-medium leading-8 text-blue-500'>
 							<span>Twitter</span>
 							<ArrowTopRightIcon className='ml-1 mt-1 h-[18px] w-[18px]' />
 						</button>
