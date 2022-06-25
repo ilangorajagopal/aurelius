89d970ac375a1195afd82837766f8d49578c8f63
Ilango <rajagopalilango@gmail.com>
2022-06-26 00:56:20 +0530

    👌 IMPROVE: Update primitives

--- common/components/buttons/button.tsx
+++ common/components/buttons/button.tsx
@@ -12,12 +12,14 @@ const Button: FC<ButtonProps> = ({
 	padding = 'px-4 py-1',
 	shadow = '',
 	textColor = 'text-gray-200',
+	type = 'button',
 }) => {
 	return (
 		<button
-			className={`focus-visible:ring-blend-darken inline-flex justify-center rounded-md border border-transparent text-sm font-semibold hover:bg-blend-darken focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 ${className} ${bg} ${padding} ${shadow} ${textColor}`}
+			className={`focus-visible:ring-blend-darken inline-flex items-center justify-center rounded-md border border-transparent text-sm font-semibold hover:bg-blend-darken focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 ${className} ${bg} ${padding} ${shadow} ${textColor}`}
 			disabled={isDisabled}
 			onClick={onClick}
+			type={type}
 		>
 			{isLoading && (
 				<svg
